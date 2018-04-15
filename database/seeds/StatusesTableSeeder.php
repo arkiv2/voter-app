5f3f2c2ef7593ecec26b3ac130b71773c5927a1b
Arki Valencia II <arkivalenciaii@notyourtechguy.me>
2018-04-15 15:59:39 +0000

    Added new model - Status

---
+++ database/seeds/StatusesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StatusesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('statuses')->insert([
+            'name' => 'NON-STRAIGHT',
+        ]);
+
+        DB::table('statuses')->insert([
+            'name' => 'STRAIGHT',
+        ]);
+    }
+}
